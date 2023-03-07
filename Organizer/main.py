1c5416ece9ad2437e13c51ef0f1e6196da09cde8
Damian Charazka <damian.charazka1@gmail.com>
2023-03-07 18:31:52 +0100

    Added deleting note, check if the json file is empty

--- Organizer/main.py
+++ Organizer/main.py
@@ -5,6 +5,7 @@ from os import path
 
 redFont = '\x1b[38;2;255;0;0m'
 greenFont = '\x1b[38;2;124;252;0m'
+orangeFont = '\x1b[38;2;255;165;0m'
 cEnd = '\x1b[0m'
 
 filePath = 'C:/Users/Damian/Desktop/Python/Organizer/file.json'
@@ -28,6 +29,9 @@ def startMenu():
     elif userInput == "2":
         os.system('cls')
         addNote()
+    elif userInput == "3":
+        os.system('cls')
+        deleteNote()
     else:
         os.system('cls')
         startMenu()
@@ -49,6 +53,36 @@ def addNote():
         addNote()
 
 
+def deleteNote():
+    print('\n Enter cancel to cancel deleting..')
+    userInput = input('\n Enter note\'s id that you want to delete: ')
+    if userInput == 'cancel':
+        startMenu()
+    else:
+        if userInput.isdigit():
+            userInput = int(userInput)
+            with open(filePath, "r") as file:
+                listObj = json.load(file)
+                if userInput < 1 or userInput > len(listObj['tasks']):
+                    os.system('cls')
+                    print(redFont + '\n Note with given id does not exist..' + cEnd)
+                    deleteNote()
+                else:
+                    for x in listObj['tasks']:
+                        if x['id'] == userInput:
+                            listObj['tasks'].pop(userInput-1)
+                            with open(filePath, "w") as file:
+                                json.dump(listObj, file, indent=4)
+                            autoIndex()
+                            os.system('cls')
+                            print(greenFont + '\n Note deleted successfully.' + cEnd)
+                            startMenu()
+        else:
+            os.system('cls')
+            print(redFont + '\n You have to enter the id as a number..' + cEnd)
+            deleteNote()
+
+
 def editJson(task):
     isFile()
 
@@ -63,20 +97,37 @@ def editJson(task):
     startMenu()
 
 
-def showNotes():
-    isFile()
-
+def autoIndex():
     with open(filePath, "r+") as file:
         listObj = json.load(file)
         id_generator = itertools.count(1)
-        print('\n === NOTES ===')
         for x in listObj['tasks']:
             x['id'] = next(id_generator)
+        file.seek(0)
+        json.dump(listObj, file, indent=4)
+
+
+def isJsonEmpty():
+    with open(filePath, "r") as file:
+        listObj = json.load(file)
+        if len(listObj['tasks']) == 0:
+            print(orangeFont + '\n You have no notes. Write something first.' + cEnd)
+            startMenu()
+
+
+def showNotes():
+    isFile()
+    isJsonEmpty()
+    autoIndex()
+
+    print('\n === NOTES ===')
+    with open(filePath, "r") as file:
+        listObj = json.load(file)
+        for x in listObj['tasks']:
             print('\n\n Id:', x['id'])
             print('\n Title:', x['title'])
             print('\n Note:', x['note'])
-        file.seek(0)
-        json.dump(listObj, file, indent=4)
+
     input('\n\n Enter to return to menu: ')
     os.system('cls')
     startMenu()
